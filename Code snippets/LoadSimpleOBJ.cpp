688899c22f74b850f8f665725fe1d4561f469bdf
Enrico <enrico.cidadeb@gmail.com>
2026-03-28 10:10:57 -0300

    Atividades modulo 3 e 4

--- Code snippets/LoadSimpleOBJ.cpp
+++ Code snippets/LoadSimpleOBJ.cpp
@@ -1,99 +1,98 @@
-/*
- *  Codificado por Rossana Baptista Queiroz
- *  para as disciplinas de Processamento Gráfico/Computação Gráfica - Unisinos
- *  Versão inicial: 07/04/2017
- *  Última atualização: 14/03/2025
- *
- *  Este arquivo contém a função `loadSimpleOBJ`, responsável por carregar arquivos
- *  no formato Wavefront .OBJ e armazenar seus vértices em um VAO para renderização
- *  com OpenGL.
- *
- *  Forma de uso (carregamento de um .obj)
- *  -----------------
- *  ...
- *  int nVertices;
- *  GLuint objVAO = loadSimpleOBJ("../Modelos3D/Cube.obj", nVertices);
- *  ...
- *
- *  Chamada de desenho (Polígono Preenchido - GL_TRIANGLES), no loop do programa:
- *  ----------------------------------------------------------
- *  ...
- *  glBindVertexArray(objVAO);
- *  glDrawArrays(GL_TRIANGLES, 0, nVertices);
- *
- */
-
- // Cabeçalhos necessários (para esta função), acrescentar ao seu código 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
- 
- 
+
 using namespace std;
 
 // GLAD
 #include <glad/glad.h>
- 
 // GLFW
 #include <GLFW/glfw3.h>
- 
-//GLM
+// GLM
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
-struct Mesh 
+// Função auxiliar para extrair o diretório do caminho do arquivo
+string getDirectoryPath(const string& filePath) {
+    size_t pos = filePath.find_last_of("\\/");
+    return (std::string::npos == pos) ? "" : filePath.substr(0, pos + 1);
+}
+
+// Assinatura atualizada: adicionado 'texturePath' por referência
+int loadSimpleOBJ(string filePATH, int &nVertices, string &texturePath)
 {
-    GLuint VAO; 
-
-};
-
-int loadSimpleOBJ(string filePATH, int &nVertices)
- {
     std::vector<glm::vec3> vertices;
     std::vector<glm::vec2> texCoords;
     std::vector<glm::vec3> normals;
     std::vector<GLfloat> vBuffer;
-    glm::vec3 color = glm::vec3(1.0, 0.0, 0.0);
 
     std::ifstream arqEntrada(filePATH.c_str());
     if (!arqEntrada.is_open()) 
-	{
+    {
         std::cerr << "Erro ao tentar ler o arquivo " << filePATH << std::endl;
         return -1;
     }
 
+    string basePath = getDirectoryPath(filePATH);
     std::string line;
+
     while (std::getline(arqEntrada, line)) 
-	{
+    {
         std::istringstream ssline(line);
         std::string word;
         ssline >> word;
 
-        if (word == "v") 
-		{
+        // 1. Busca pelo arquivo de material
+        if (word == "mtllib") 
+        {
+            string mtlFile;
+            ssline >> mtlFile;
+            string mtlPath = basePath + mtlFile;
+            
+            std::ifstream mtlEntrada(mtlPath.c_str());
+            if (mtlEntrada.is_open()) {
+                string mtlLine;
+                while (std::getline(mtlEntrada, mtlLine)) {
+                    std::istringstream mtlSS(mtlLine);
+                    string mtlWord;
+                    mtlSS >> mtlWord;
+                    
+                    // Encontra a textura difusa
+                    if (mtlWord == "map_Kd") {
+                        mtlSS >> texturePath;
+                        // Anexa o caminho do diretório para carregar a imagem corretamente depois
+                        texturePath = basePath + texturePath; 
+                        break; 
+                    }
+                }
+                mtlEntrada.close();
+            } else {
+                std::cerr << "Aviso: Nao foi possivel abrir o arquivo MTL " << mtlPath << std::endl;
+            }
+        }
+        else if (word == "v") 
+        {
             glm::vec3 vertice;
             ssline >> vertice.x >> vertice.y >> vertice.z;
             vertices.push_back(vertice);
         } 
         else if (word == "vt") 
-		{
+        {
             glm::vec2 vt;
             ssline >> vt.s >> vt.t;
             texCoords.push_back(vt);
         } 
         else if (word == "vn") 
-		{
+        {
             glm::vec3 normal;
             ssline >> normal.x >> normal.y >> normal.z;
             normals.push_back(normal);
         } 
         else if (word == "f")
-		 {
+        {
             while (ssline >> word) 
-			{
+            {
                 int vi = 0, ti = 0, ni = 0;
                 std::istringstream ss(word);
                 std::string index;
@@ -102,12 +101,19 @@ int loadSimpleOBJ(string filePATH, int &nVertices)
                 if (std::getline(ss, index, '/')) ti = !index.empty() ? std::stoi(index) - 1 : 0;
                 if (std::getline(ss, index)) ni = !index.empty() ? std::stoi(index) - 1 : 0;
 
+                // Armazena Posição (x, y, z)
                 vBuffer.push_back(vertices[vi].x);
                 vBuffer.push_back(vertices[vi].y);
                 vBuffer.push_back(vertices[vi].z);
-                vBuffer.push_back(color.r);
-                vBuffer.push_back(color.g);
-                vBuffer.push_back(color.b);
+                
+                // Armazena Coordenada de Textura (s, t) - Trata casos de OBJ sem textura
+                if (!texCoords.empty()) {
+                    vBuffer.push_back(texCoords[ti].s);
+                    vBuffer.push_back(texCoords[ti].t);
+                } else {
+                    vBuffer.push_back(0.0f);
+                    vBuffer.push_back(0.0f);
+                }
             }
         }
     }
@@ -123,16 +129,21 @@ int loadSimpleOBJ(string filePATH, int &nVertices)
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    // 2. Configuração dos Atributos de Vértice (agora com stride de 5)
+    
+    // Atributo 0: Posição (x, y, z) - 3 floats
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    // Atributo 1: Coordenada de Textura (s, t) - 2 floats, offset de 3 floats
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-	nVertices = vBuffer.size() / 6;  // x, y, z, r, g, b (valores atualmente armazenados por vértice)
+    // 3. Atualização do número de vértices (x, y, z, s, t)
+    nVertices = vBuffer.size() / 5;  
 
     return VAO;
 }
